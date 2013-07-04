6d63051cf8183ee8a9153507dbdc76ac9fcaf771
rezajatnika <rezajatnika@gmail.com>
2013-07-04 15:27:31 +0700

    Add ask question form

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Makeitsnappy::Application.routes.draw do
 
   root to: 'questions#index'
 
-  resources :users,    only: [:new, :create]
-  resources :sessions, only: [:new, :create]
+  resources :users,     only:   [:new, :create]
+  resources :sessions,  only:   [:new, :create]
+  resources :questions, except: [:new]
 
   match '/register', to: 'users#new'
   match '/login',    to: 'sessions#new'
